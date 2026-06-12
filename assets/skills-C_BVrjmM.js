a09af14c12a7310a5ad2472dd11a12696284ac1a
GannaGamal <gannagamal396@gmail.com>
2026-06-12 23:12:45 +0300

    Updates

---
+++ assets/skills-C_BVrjmM.js
@@ -0,0 +1,2 @@
+const i=["JavaScript","React","Python","Java","Node.js","TypeScript","UI/UX Design","Graphic Design","Content Writing","SEO","Mobile Development","WordPress","Shopify","Video Editing","Social Media","Data Analysis","Machine Learning","AWS"];export{i as P};
+//# sourceMappingURL=skills-C_BVrjmM.js.map
